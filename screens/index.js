aee5c4fd09fd4df34439d8c0b30cb3b280bdb6dc
Tokismos <tokismos@gmail.com>
2021-11-06 03:29:57 +0100

    add other forms

--- screens/index.js
+++ screens/index.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   Dimensions,
   ScrollView,
+  CheckBox,
 } from "react-native";
 
 import { RadioButton, TextInput } from "react-native-paper";
@@ -72,11 +73,27 @@ let arrayIngredients = [
     quantity: "",
   },
 ];
+const CATEGORIES = ["Viande", "Poisson", "Végétarien", "Vegan", "Sans Gluten"];
+// let catArray = [];
+
 const index = () => {
   const [imageURL, setImgURL] = React.useState("");
   const [disabled, setDisabled] = React.useState(false);
   const [name, setName] = React.useState("");
+  const [nbrPersonne, setNbrPersonne] = React.useState("");
+  const [tempsCuisson, setTempsCuisson] = React.useState("");
+  const [tempsPreparation, setTempsPreparation] = React.useState("");
   const [difficulty, setDifficulty] = React.useState("");
+  const [isMeat, setIsMeat] = React.useState(false);
+  const [isFish, setIsFish] = React.useState(false);
+  const [isVegetarien, setIsVegetarien] = React.useState(false);
+  const [isVegan, setIsVegan] = React.useState(false);
+  const [isNoGluten, setIsNoGluten] = React.useState(false);
+  const [four, setFour] = React.useState(false);
+  const [microOnde, setMicroOnde] = React.useState(false);
+  const [mixeur, setIsMixeur] = React.useState(false);
+  const [robotCuiseur, setIsRobotCuiseur] = React.useState(false);
+  const [friteuse, setIsFriteuse] = React.useState(false);
   const [ingredients, setIngredients] = React.useState([
     {
       name: "",
@@ -105,7 +122,108 @@ const index = () => {
     setSteps(steps.toString());
     arrayIngredients = ingredients;
   };
-  const addRecipe = async (imgURL, name, difficulty, steps, ingredients) => {
+  const CategoryComponent = () => {
+    const [isSelected, setSelection] = useState(false);
+    const CategoryItem = ({ value, set, name }) => {
+      return (
+        <View style={{ margin: 4 }}>
+          <TouchableOpacity
+            style={styles.difficultyItemsTouchable}
+            onPress={() => {
+              set((prev) => !prev);
+            }}
+          >
+            <Text style={{ marginVertical: 10 }}>{name}</Text>
+
+            <CheckBox value={value} style={styles.checkbox} />
+          </TouchableOpacity>
+        </View>
+      );
+    };
+    return (
+      <View style={styles.difficultyContainer}>
+        <View style={{ width: "20%" }}>
+          <Text style={styles.difficultyText}>Categorie</Text>
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          <CategoryItem value={isMeat} set={setIsMeat} name="Viande" />
+          <CategoryItem value={isFish} set={setIsFish} name="Poisson" />
+          <CategoryItem
+            value={isVegetarien}
+            set={setIsVegetarien}
+            name="Vegetarien"
+          />
+          <CategoryItem value={isVegan} set={setIsVegan} name="Vegan" />
+          <CategoryItem
+            value={isNoGluten}
+            set={setIsNoGluten}
+            name="Sans Gluten"
+          />
+        </View>
+      </View>
+    );
+  };
+  const MaterielComponent = () => {
+    const [isSelected, setSelection] = useState(false);
+    const MaterielItem = ({ value, set, name }) => {
+      return (
+        <View style={{ margin: 4 }}>
+          <TouchableOpacity
+            style={styles.difficultyItemsTouchable}
+            onPress={() => {
+              set((prev) => !prev);
+            }}
+          >
+            <Text style={{ marginVertical: 10 }}>{name}</Text>
+
+            <CheckBox value={value} style={styles.checkbox} />
+          </TouchableOpacity>
+        </View>
+      );
+    };
+    return (
+      <View style={styles.difficultyContainer}>
+        <View style={{ width: "20%" }}>
+          <Text style={styles.difficultyText}>Materiel</Text>
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          <MaterielItem value={four} set={setFour} name="Four" />
+          <MaterielItem
+            value={microOnde}
+            set={setMicroOnde}
+            name="Micro-Onde"
+          />
+          <MaterielItem value={mixeur} set={setIsMixeur} name="Mixeur" />
+          <MaterielItem
+            value={robotCuiseur}
+            set={setIsRobotCuiseur}
+            name="Robot Cuiseur"
+          />
+          <MaterielItem value={friteuse} set={setIsFriteuse} name="Friteuse" />
+        </View>
+      </View>
+    );
+  };
+  const addRecipe = async (
+    imgURL,
+    name,
+    difficulty,
+    steps,
+    nbrPersonne,
+    tempsCuisson,
+    tempsPreparation,
+    ingredients,
+    four,
+    microOnde,
+    mixeur,
+    robotCuiseur,
+    friteuse,
+    isMeat,
+    isFish,
+    isVegetarien,
+    isVegan,
+    isNoGluten
+  ) => {
     const stepsToArray = steps.split(/\r?\n/);
     const stepsWithoutSpace = stepsToArray.filter(
       //detect the white spaces in a line
@@ -117,7 +235,20 @@ const index = () => {
       name,
       difficulty,
       steps: stepsWithoutSpace,
+      nbrPersonne: +nbrPersonne,
+      tempsCuisson: +tempsCuisson,
+      tempsPreparation: +tempsPreparation,
       ingredients,
+      four,
+      microOnde,
+      mixeur,
+      robotCuiseur,
+      friteuse,
+      isMeat,
+      isFish,
+      isVegetarien,
+      isVegan,
+      isNoGluten,
     };
     // console.log("steps", steps);
     // console.log("in", ingredients);
@@ -165,11 +296,39 @@ const index = () => {
           onChangeText={setName}
           style={styles.url}
         />
+        <View
+          style={{
+            width: "70%",
+            flexDirection: "row",
+            justifyContent: "space-around",
+          }}
+        >
+          <TextInput
+            label="Nb.Personne"
+            value={nbrPersonne}
+            onChangeText={setNbrPersonne}
+            style={([styles.url], { flex: 1 / 4 })}
+          />
+          <TextInput
+            label="T.Cuisson"
+            value={tempsCuisson}
+            onChangeText={setTempsCuisson}
+            style={([styles.url], { flex: 1 / 4 })}
+          />
+          <TextInput
+            label="T.Preparation  "
+            value={tempsPreparation}
+            onChangeText={setTempsPreparation}
+            style={([styles.url], { flex: 1 / 4 })}
+          />
+        </View>
         {/* INGREDIENT VIEW */}
         <DifficultyComponent
           difficulty={difficulty}
           setDifficulty={setDifficulty}
         />
+        <CategoryComponent />
+        <MaterielComponent />
         {/* ETAPES VIEW */}
         <View style={styles.multiLine}>
           <TextInput
@@ -208,7 +367,26 @@ const index = () => {
               justifyContent: "center",
             }}
             onPress={() =>
-              addRecipe(imageURL, name, difficulty, steps, arrayIngredients)
+              addRecipe(
+                imageURL,
+                name,
+                difficulty,
+                steps,
+                nbrPersonne,
+                tempsCuisson,
+                tempsPreparation,
+                arrayIngredients,
+                four,
+                microOnde,
+                mixeur,
+                robotCuiseur,
+                friteuse,
+                isMeat,
+                isFish,
+                isVegetarien,
+                isVegan,
+                isNoGluten
+              )
             }
           >
             <Text style={{}}>Add To DataBase</Text>
